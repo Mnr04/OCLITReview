ccd4f657a33b307cbfafdec07953260adc4e344f
Mnr04 <181124438+Mnr04@users.noreply.github.com>
2025-12-19 16:50:23 +0100

    Authentification

--- litrevu/urls.py
+++ litrevu/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from reviews import views
 from django.contrib.auth.views import LoginView
 
 urlpatterns = [
+    path('', views.main, name='main'),
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(template_name='reviews/login.html'), name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('logout/', views.log_out, name='logout'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout
 
-# Create your views here.
+class SignupForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        # Tell Django to use this Model to save data
+        model = get_user_model()
+        # And show this fields
+        fields = ('username',)
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # stick session cookie to user to have his information
+            login(request, user)
+            return redirect('main')
+    else:
+        form = SignupForm()
+
+    return render(request, 'reviews/signup.html', {'form': form})
+
+def main(request):
+    return render(request, 'reviews/main.html')
+
+def log_out(request):
+    logout(request)
+    return redirect('login')
