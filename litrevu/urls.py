12a422a509cea724685846be1650a18b53ecfc14
Mnr04 <181124438+Mnr04@users.noreply.github.com>
2025-12-21 11:21:38 +0100

    Review feature + code Review / Bug correction

--- litrevu/urls.py
+++ litrevu/urls.py
@@ -10,5 +10,12 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('logout/', views.log_out, name='logout'),
 
-    path('createTicket', views.createTicket, name="createTicket")
+    path('create_ticket', views.create_ticket, name="create_ticket"),
+    path('ticket/<int:ticket_id>/edit/', views.edit_ticket, name='edit_ticket'),
+    path('ticket/<int:ticket_id>/delete/', views.delete_ticket, name='delete_ticket'),
+
+    path('create_ticket_and_review', views.create_ticket_and_review, name="create_ticket_and_review"),
+    path('review/<int:ticket_id>/create_review/', views.create_review, name='create_review'),
+    path('review/<int:review_id>/edit/', views.edit_review, name='edit_review'),
+    path('review/<int:review_id>/delete/', views.delete_review, name='delete_review'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import logout
 from django import forms
-from .models import Ticket
+from .models import Ticket, Review
 
 class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
@@ -17,6 +17,17 @@ class TicketForm(forms.ModelForm):
         model = Ticket
         fields = ['title', 'description', 'image']
 
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['headline', 'rating', 'body']
+        labels = {
+            'headline': 'Titre',
+            'rating': 'Note (0 à 5)',
+            'body': 'Commentaire',
+        }
+
+""" Authentification """
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
@@ -37,7 +48,8 @@ def log_out(request):
     logout(request)
     return redirect('login')
 
-def createTicket(request):
+""" Ticket """
+def create_ticket(request):
     if request.method == 'POST':
         form = TicketForm(request.POST, request.FILES)
         if form.is_valid():
@@ -48,4 +60,106 @@ def createTicket(request):
     else:
         form = TicketForm()
 
-    return render(request, 'reviews/createTicket.html', {'form': form})
+    return render(request, 'reviews/create_ticket.html', {'form': form})
+
+def edit_ticket(request, ticket_id):
+    ticket = get_object_or_404(Ticket, id=ticket_id)
+
+    if ticket.user != request.user:
+        return redirect('main')
+
+    if request.method == 'POST':
+        form = TicketForm(request.POST, request.FILES, instance=ticket)
+        if form.is_valid():
+            form.save()
+            return redirect('main')
+    else:
+        form = TicketForm(instance=ticket)
+
+    return render(request, 'reviews/edit_ticket.html', {
+        'form': form,
+        'ticket': ticket}
+        )
+
+def delete_ticket(request, ticket_id):
+    ticket = get_object_or_404(Ticket, id=ticket_id)
+
+    if ticket.user == request.user:
+        if request.method == 'POST':
+            ticket.delete()
+            return redirect('main')
+
+    return redirect('main')
+
+""" Review """
+def create_review(request, ticket_id):
+    ticket = get_object_or_404(Ticket, id=ticket_id)
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.ticket = ticket
+            review.user = request.user
+            review.save()
+            return redirect('main')
+    else:
+        form = ReviewForm()
+
+    return render(request, 'reviews/create_review.html', {'form': form, 'ticket': ticket})
+
+def create_ticket_and_review(request):
+    if request.method == 'POST':
+        ticket_form = TicketForm(request.POST, request.FILES)
+        review_form = ReviewForm(request.POST)
+
+        if ticket_form.is_valid() and review_form.is_valid():
+            ticket = ticket_form.save(commit=False)
+            ticket.user = request.user
+            ticket.save()
+
+            review = review_form.save(commit=False)
+            review.ticket = ticket
+            review.user = request.user
+            review.save()
+
+            return redirect('main')
+
+    else:
+        ticket_form = TicketForm()
+        review_form = ReviewForm()
+
+    context = {
+        'ticket_form': ticket_form,
+        'review_form': review_form,
+    }
+    return render(request, 'reviews/create_ticket_and_review.html', context)
+
+def edit_review(request, review_id):
+    review = get_object_or_404(Review, id=review_id)
+
+    if review.user != request.user:
+        return redirect('main')
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, request.FILES, instance=review)
+        if form.is_valid():
+            form.save()
+            return redirect('main')
+    else:
+        form = ReviewForm(instance=review)
+
+    return render(request, 'reviews/edit_review.html', {
+        'form': form,
+        'review': review,
+        'ticket': review.ticket}
+        )
+
+def delete_review(request, review_id):
+    review = get_object_or_404(Review, id=review_id)
+
+    if review.user == request.user:
+        review.delete()
+        return redirect('main')
+
+    return redirect('main')
